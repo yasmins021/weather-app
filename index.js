1fa89741263f820873950ea4497e9a771907f852
yasmins021 <109712172+yasmins021@users.noreply.github.com>
2022-08-03 13:57:38 -0700

    Added daily forecast function

--- index.js
+++ index.js
@@ -27,7 +27,37 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-
+function displayForcast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+              <div class="col-2">
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
+              
+                  <img src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png" alt="" width="42"/>
+                  <div class="weather-forecast-temperature">
+                    <span class="weather-forecast-temperature-max">${Math.round(
+                      forecastDay.temp.max
+                    )}°</span>
+                    <span class="weather-forecast-temperature-min">${Math.round(
+                      forecastDay.temp.min
+                    )}°</span>
+                   </div>
+                </div> `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 
 
